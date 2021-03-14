fb328ebfca1e3d2ce7b7564b6c65b8756fee6e81
William Prudencio <36481731+wprudencio97@users.noreply.github.com>
2021-03-14 19:52:50 -0400

    Added menu items model.

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+
+    FOOD_CATEGORIES = [
+        ('OTHER', 'Other'),
+        ('APPETIZERS', 'Appetizers'),
+        ('SOUP', 'Soup'),
+        ('FRIED_RICE', 'Fried Rice')
+    ]
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    category = models.CharField(max_length=30, choices=FOOD_CATEGORIES, default=FOOD_CATEGORIES[0])
+    picture = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return f'{self.name}'
